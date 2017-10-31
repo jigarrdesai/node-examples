71985ddcdfa446c0376cd781adf966fae7876b61
jigar desai <jigardesai@jigars-MacBook-Pro.local>
2017-10-31 16:59:21 +0530

    changes

---
+++ index.js
@@ -0,0 +1,16 @@
+var react = require('./rectangle');
+
+function solveRect(l,b){
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+    if(l <= 0 || b <= 0 ){
+        console.log("Rectangle dimensions should be greather than zero : l = " + l + ", and b = " + b);
+    } else {
+        console.log("The area of the rectangle is " + react.area(l,b));
+        console.log("The perimeter of the rectangle is "+ react.perimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
